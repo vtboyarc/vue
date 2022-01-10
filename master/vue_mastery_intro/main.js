106aef7a2c2d3f79b034acf88026a438ded179a8
vtboyarc <adamrcarter@gmail.com>
2022-01-10 17:21:08 -0600

    more vue

--- master/vue_mastery_intro/main.js
+++ master/vue_mastery_intro/main.js
@@ -2,7 +2,19 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'A warm fuzzy pair of smartwool socks.' 
+            image: './assets/images/socks_green.jpeg',
+            url: 'https://www.adamcarter.io',
+            inventory: 15,
+            onSale: true,
+            details: [
+                '50% cotton',
+                '30% wool',
+                '20% polyester'
+            ],
+            variants: [
+                { id: 2234, color: 'green'},
+                { id: 2235, color: 'blue'}
+            ]
         }
     }
 })
